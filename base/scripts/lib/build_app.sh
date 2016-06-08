64c1304548f15a7ae417e960b31a51c6043ce916
zhaoyao91 <zhaoyao91@163.com>
2016-06-08 14:43:22 +0800

    base on zhaoyao91/meteord:base

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -8,10 +8,7 @@ BUNDLE_DIR=/tmp/bundle-dir
 cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
 
-echo 'install npm dependencies ~~~~~~~~~~~~~~~~~~~~~'
 npm install --production
-echo 'finished ~~~~~~~~~~~~~~~~~~~~~'
-
 meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
 
 cd $BUNDLE_DIR/bundle/programs/server/
