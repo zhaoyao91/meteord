4b1993d6008cb6306ad516dd550d37713b284d60
zhaoyao91 <zhaoyao91@163.com>
2016-06-08 14:00:20 +0800

    install npm packages before building

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -8,6 +8,7 @@ BUNDLE_DIR=/tmp/bundle-dir
 cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
 
+npm install --production
 meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
 
 cd $BUNDLE_DIR/bundle/programs/server/
