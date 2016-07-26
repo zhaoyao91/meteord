c280470b11e91378e594fda467f0a2caa3310823
zhaoyao91 <zhaoyao91@163.com>
2016-07-26 22:52:14 +0800

    update npm

--- binbuild/scripts/install_binbuild_tools.sh
+++ binbuild/scripts/install_binbuild_tools.sh
@@ -1,6 +1,10 @@
 set -e
 # for npm module re-building
 apt-get -y install build-essential libssl-dev git python
+
+npm i -g npm
+echo npm -v
+
 npm install -g node-gyp
 # pre-install node source code for faster building
 node-gyp install ${NODE_VERSION}
